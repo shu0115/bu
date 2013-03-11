23dcfca47a14b28a9e9f06944d48bb68114db89e
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-03-11 21:40:41 +0900

    comments_controllerのフィファクタリング

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,8 +3,12 @@ require 'spec_helper'
 
 describe CommentsController do
   describe "GET show" do
-    let(:comment) { FactoryGirl.create(:comment) }
-    before { get :show, id: comment.to_param }
+    let(:you) { FactoryGirl.create(:user) }
+    let(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
+    let(:event) { FactoryGirl.create(:event, group_id: group.id) }
+    let!(:comment) { FactoryGirl.create(:comment, event_id: event.id) }
+
+    before { get :show, id: comment.to_param, group_id: group.to_param, event_id: event.to_param }
     it { assigns(:comment).should eq comment }
   end
 
@@ -13,20 +17,21 @@ describe CommentsController do
 
     context 'グループメンバーの場合' do
       let(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
-      let!(:event) { FactoryGirl.create(:event, group_id: group.id) }
-      let(:comment) { FactoryGirl.attributes_for(:comment, event_id: event.id) }
+      let(:event) { FactoryGirl.create(:event, group_id: group.id) }
+      let!(:comment) { FactoryGirl.attributes_for(:comment, event_id: event.id) }
 
       before { login_as(you) }
 
       context 'valid params' do
-         before { post :create, {:comment => comment} }
-         it { response.should redirect_to(assigns(:comment).event) }
+        let(:url) { group_event_url(group_id: assigns(:comment).event.group.id, id: assigns(:comment).event.id) }
+         before { post :create, {comment: comment, group_id: group.to_param, event_id: event.to_param} }
+         it { response.should redirect_to(url) }
        end
 
       context 'invalid params' do
         before do
           Comment.any_instance.should_receive(:save) { false }
-          post :create, {:comment => comment}
+          post :create, {comment: comment, group_id: group.to_param, event_id: event.to_param}
         end
 
         it { response.should render_template("new") }
@@ -44,12 +49,24 @@ describe CommentsController do
         bypass_rescue
       end
 
-      it { expect{ post :create, comment: comment }.to raise_error(Group::NotGroupMember) }
+      it "Group::NotGroupMemberエラーが発生すること" do
+        expect {
+          post :create, comment: comment, group_id: group.to_param, event_id: event.to_param
+        }.to raise_error(Group::NotGroupMember)
+      end
     end
   end
 
   describe "DELETE destroy" do
-    let!(:comment) { FactoryGirl.create(:comment) }
-    it { expect { delete :destroy, id: comment.to_param }.to change(Comment, :count).by(-1) }
+    let(:you) { FactoryGirl.create(:user) }
+    let(:group) { FactoryGirl.create(:group, owner_user_id: you.id) }
+    let(:event) { FactoryGirl.create(:event, group_id: group.id) }
+    let!(:comment) { FactoryGirl.create(:comment, event_id: event.id) }
+
+    it "1件減っていること" do
+      expect {
+        delete :destroy, group_id: group.to_param, event_id: event.to_param, id: comment.to_param
+      }.to change(Comment, :count).by(-1)
+    end
   end
 end
