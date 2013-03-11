65c6b00f1aa3e3882ba58f54c205f552d7bcb481
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-03-11 21:42:49 +0900

    users_controller/my_controllerのリファクタリング

---
+++ spec/controllers/my_controller_spec.rb
@@ -0,0 +1,50 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe MyController do
+  describe "GET 'edit'" do
+    context 'ログインしている場合' do
+      let(:you) { FactoryGirl.create(:user) }
+      before do
+        login_as(you)
+        get :edit
+      end
+      it { assigns(:user).should eq you }
+    end
+
+    context 'ログインしてない場合' do
+      before { bypass_rescue }
+      it { expect { get :edit }.to raise_error(User::UnAuthorized) }
+    end
+  end
+
+  describe "PUT 'update'" do
+    context 'ログインしている場合' do
+      let(:you) { FactoryGirl.create(:user) }
+      before { login_as(you) }
+
+      context 'valid params' do
+        before do
+          User.any_instance.should_receive(:update_attributes).with('these' => 'params').and_return { true }
+          put :update, {user: {'these' => 'params'}}
+        end
+
+        it { response.should redirect_to(my_url) }
+      end
+
+      context 'invalid params' do
+        before do
+          User.any_instance.should_receive(:update_attributes).with('these' => 'params').and_return { false }
+          put :update, {user: {'these' => 'params'}}
+        end
+
+        it { response.should render_template("edit") }
+      end
+    end
+
+    context 'ログインしてない場合' do
+      before { bypass_rescue }
+      it { expect { get :edit }.to raise_error(User::UnAuthorized) }
+    end
+  end
+end
