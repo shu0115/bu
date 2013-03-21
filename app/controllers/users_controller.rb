764016cf0b9555316db9a456ea167d84789f7607
TAKAHASHI Kazunari <takahashi@1syo.net>
2013-03-21 20:19:03 +0900

    fix users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  skip_before_filter :require_current_user
+
   before_filter :login_required, :only => [:edit, :update]
   # GET /users/1
   def show
-    @current_user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
